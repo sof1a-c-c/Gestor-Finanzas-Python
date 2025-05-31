40fc48ce3817fea3ac5124fd7afde757dc50b3dc
Sof <is.patino15@ciaf.edu.co>
2025-05-31 15:57:34 -0500

    Documenté el código, hice correcciones y validaciones

--- GestorFinanzas.py
+++ GestorFinanzas.py
@@ -1,11 +1,16 @@
-import os
+import os #para usarla en la funcoión de limpiarConsola
 
+#Categorías para gastos
 categorias = ["Alimentación", "Transporte", "Entretenimiento", "Salud", "Ropa", "Hogar", "Educación", "Viajes", "Mascotas"]
 saldo = 0.0
+
+#Lista para guardar las transacciones que se hacen (guarda diccionarios)
 transacciones = []
 
+# Limpia la consola dependiendo del sistema operativo
+# 'cls' para Windows, 'clear' para Unix (Linux/macOS)
 def limpiarConsola():
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")   
 
 def Enter():
     input("Presione Enter para continuar...")
@@ -16,14 +21,17 @@ def guardarTransaccion(tipo, monto, categoria):
 
     global saldo
 
+    #definimos el diccionario
     transaccion = {
         "tipo": tipo,
         "monto": monto,
         "categoria": categoria
     }
 
+    #Guardamos el diccionario transaccion a la lista transacciones
     transacciones.append(transaccion)
 
+    #Según el tipo de transacción (ingreso/gasto) se le aumenta o descuenta al saldo
     if tipo == "+":
         saldo += monto
     elif tipo == "-":
@@ -32,39 +40,74 @@ def guardarTransaccion(tipo, monto, categoria):
 
 
 def agregarGasto():
-    
-    monto = float(input("Ingrese el monto: "))
+
+    limpiarConsola()
+    print("---✨ Agregar Gasto ✨---\n")
+
+    while True:
+        try:
+            monto = float(input("Ingrese el monto: "))
+            if monto < 0: #Se verifica que el usuario ponga valores positivos
+                print("✖️ Solo se aceptan valores positivos")
+            else:
+                break
+        except ValueError:
+            print("Solo se permiten números 👀")
+
+
     print("Categorías disponibles:")
 
+    #imprime la lista que tenemos de categorias
     for i in range(len(categorias)):
         print(f"{i + 1}. {categorias[i]}")
 
-    try:
+
+    while True:
+    
+        try:
+            categoria = int(input("Seleccione la categoría (número): ")) - 1 #restamos 1 porque en el momento que mostramos las categorías con su respectivo número le sumamos 1, entonces se le resta 1 para acceder a la posición correcta de la lista categorias
+
+            if categoria < 0 or categoria >= len(categorias): #Verificamos que el usuario ponga una categoría existente
+                print("Categoría no válida. Intentelo de nuevo")
+              
+            else:
+                #imprimimos el monto y la categoría
+                print(f"Monto: {monto}\nCategoría: {categorias[categoria]}")
+
+                #Guardamos la transacción
+                guardarTransaccion("-", monto, categorias[categoria])
+                Enter()
+                break
+
+        except ValueError:
+            #Verificamos que el usuario solo ponga números
+            print("Solo se permiten números 👀")
+            Enter()
         
-        categoria = int(input("Seleccione la categoría (número): ")) - 1
-            
-
-        if categoria < 0 or categoria >= len(categorias):
-            print("Categoría no válida.")
-            return
-        else:
-            print(f"Monto: {monto}\nCategoría: {categorias[categoria]}")
-
-        guardarTransaccion("-", monto, categorias[categoria])
-        Enter()
-
-    except ValueError:
-        print("Solo se permiten números 👀")
-        Enter()
-        
-
 
 
 def agregarIngreso():
-    monto = float(input("Ingrese el monto: "))
-    print(f"Monto: {monto}")
-    guardarTransaccion("+", monto, "Ingreso")
-    Enter()
+
+    limpiarConsola()
+
+    print("---✨ Agregar Ingreso ✨---\n")
+
+    while True:
+        try:
+            monto = float(input("Ingrese el monto: "))
+            if monto < 0: #Se verifica que el usuario ponga valores positivos
+                print("✖️ Solo se aceptan valores positivos")
+            else:
+                #Mostramos el monto y guardamos la transacción
+                print(f"Monto: {monto}")
+                guardarTransaccion("+", monto, "Ingreso")
+                Enter()
+                break
+        except ValueError:
+            #Verificamos que el usuario solo ponga números
+            print("Solo se permiten números 👀")
+
+    
 
 
 
@@ -73,6 +116,7 @@ def consultarSaldo():
 
     limpiarConsola()
     if saldo < 0:
+        #Significa que el usuario debe dinero, la función abs muestra el valor absoluto del saldo
         print(f"Pilas, debes ${abs(saldo)} 👀")
     else:
         print(f"Saldo actual: ${saldo}")
@@ -85,13 +129,18 @@ def historial(tipo):
 
     global transacciones
 
+    #Definimos el título del historial dependiendo del tipo de transacción
     if tipo == "-":
         tipoTransaccion = "Gastos"
     elif tipo == "+":
         tipoTransaccion = "Ingresos"
 
-    print(f"\n---✨ Historial de {tipoTransaccion} ✨---\n")
+    print(f"---✨ Historial de {tipoTransaccion} ✨---")
+
+    #recorremos la lista de transacciones para filtrar solo las del tipo solicitado
     for transaccion in transacciones:
+
+        #acá mostramos el tipo de transacción dependiendo de lo que tenemos en el diccionario si el tipo de la transacción coincide con el tipo recibido (gasto o ingreso)
         if transaccion["tipo"] == tipo:
             print(f"🔸 Categoría: {transaccion['categoria']}, Monto: {transaccion['monto']}")
 
@@ -100,51 +149,101 @@ def historial(tipo):
 
 
 def verHistorial():
-
-    limpiarConsola()    
     
     global transacciones
 
-    print("---✨ Historial de Transacciones ✨---\n")
-    print("1. Gastos\n2. Ingresos\n3. General")
-
-    try:
-        
-        opcion = int(input("Seleccione una opción: "))
-        if opcion < 1 or opcion > 3:
-            print("Opción no válida.")
-            return
-        elif opcion == 1:
-            historial("-")
-        elif opcion == 2:
-            historial("+")
-        elif opcion == 3:
+    while True:
+        try:
             limpiarConsola()
-            print("\n---✨ Historial General ✨---")
-            for transaccion in transacciones:
-                print(f"🔸 Categoría: {transaccion['categoria']}, Monto: {transaccion['monto']}")
+            print("---✨ Historial de Transacciones ✨---\n")
+            print("1. Gastos\n2. Ingresos\n3. General\n4. Volver al menú principal")
+            opcion = int(input("Seleccione una opción: "))
+            
+            #Llamamos la funcion historial con su parámetro correspondiente
+            if opcion == 1:
+                historial("-")
+            elif opcion == 2:
+                historial("+")
+            elif opcion == 3:
+                limpiarConsola()
+                print("\n---✨ Historial General ✨---")
+
+                #mostramos todas las transacciones de la lista
+                for transaccion in transacciones:
+                    print(f"🔸 Categoría: {transaccion['categoria']}, Monto: {transaccion['monto']}")
+                Enter()
+            elif opcion == 4:
+                #Salimos del bucle para vovler al menu principal
+                break
+            else:
+                print("✖️ Opción no válida. Ingrese un número del 1 al 4.")
+                Enter()
+
+        except ValueError:
+            #Verificamos que el usuario ponga solo números
+            print("👀 Solo se permiten números.")
             Enter()
 
-    except ValueError:
-        print("Solo se permiten números 👀")  
-        Enter()      
+
+
+def Categorias():
+    global categorias
+    while True:
+        limpiarConsola()
+        print("\n---✨ Categorias ✨---")
+        print("1. Ver categorías\n2. Agregar Categoría\n3. Volver al menú principal")
+        try:
+            opcion = int(input("Seleccione una opción: "))
+            if opcion == 1:
+                limpiarConsola()
+                #imprimimos todas las categorias de la lista
+                print("Categorías disponibles:")
+                for categoria in categorias:
+                    print(f"🔸 {categoria}")
+                Enter()
+
+            elif opcion == 2:
+                agregarCategoria() 
+
+            elif opcion == 3:
+                break  
+            else:
+                print("✖️ Opción no válida. Ingrese un número del 1 al 3")
+                Enter()
+
+        except ValueError:
+            print("Solo se permiten números 👀")
+            Enter()   
 
 
 def agregarCategoria():
     limpiarConsola()
+
+    #imprimimos todas las categorías 
     global categorias
     print("Categorías disponibles:")
     for i in categorias:
         print(f"🔸 {i}")
 
+    while True:
+        categoria = input("Ingrese el nombre de la nueva categoría: ")
 
-    categoria = input("Agregue la categoría: ")
-    while categoria == "":
-        categoria = input("Categoría inválida ✖️, intente de nuevo: ")
-    
-    categorias.append(categoria)
-    print("Categoría agregada exitosamente")
-    Enter()
+        #verificamos que no sea una cadena vacía
+        if categoria == "":
+            print("✖️ Categoría inválida. No puede estar vacía.")
+
+        #verificamos que no ponga una categoría existente
+        elif categoria in categorias:
+            print("⚠️ Esa categoría ya existe. Intente con otro nombre.")
+
+        #Guardamos la categoría nueva en la lista categorías
+        else:
+            categorias.append(categoria)
+            print(f"✅ Categoría '{categoria}' agregada exitosamente.")
+            Enter()
+            break  # Salimos del bucle solo si todo salió bien
+
+        Enter()
 
 
 
@@ -152,12 +251,11 @@ def menu():
 
     global categorias
 
-    
     while True:
         try:
             limpiarConsola()
             print("\n---✨ Menú Principal ✨---")
-            print("1. Agregar gasto\n2. Agregar ingreso\n3. Consultar saldo actual\n4. Ver historial\n5. Agregar Categoría\n6. Salir")
+            print("1. Agregar gasto\n2. Agregar ingreso\n3. Consultar saldo actual\n4. Ver historial\n5. Categorías\n6. Salir")
 
 
             opcion = int(input("Seleccione una opción: "))
@@ -175,7 +273,7 @@ def menu():
             elif opcion == 4:
                 verHistorial()
             elif opcion == 5:
-                agregarCategoria()
+                Categorias()
             elif opcion == 6:
                 print("Saliendo del programa...")
                 exit(0)
